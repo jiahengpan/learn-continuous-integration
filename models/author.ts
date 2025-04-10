61ba784628494bc25c6c9e4b51c1638deb678351
Jiaheng Pan <jiahengpan12.15@gmail.com>
2025-04-10 09:38:07 -0400

    this is main

--- models/author.ts
+++ models/author.ts
@@ -7,7 +7,7 @@ import mongoose, { Schema, Document, Model, FilterQuery } from 'mongoose';
  * @property date_of_birth - The author's date of birth.
  * @property date_of_death - The author's date of death.
  * @property name - The author's full name.
- * @property lifespan - The author's lifespan. 'a small change to be removed'
+ * @property lifespan - The author's lifespan. 'this is main'
  */
 export interface IAuthor extends Document {
   first_name: string;
